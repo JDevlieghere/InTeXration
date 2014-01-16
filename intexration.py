94220d727e344e9a3687b8441ff9440ea6d71d0b
JDevlieghere <info@jonasdevlieghere.com>
2014-01-16 18:15:19 +0100

    Config moved to folder.

--- intexration.py
+++ intexration.py
@@ -8,10 +8,11 @@ def main():
     # parser.add_argument('-host', help='hostname', default='localhost')
     # parser.add_argument('-port', help='port', default=8000)
     # args = parser.parse_args()
+    path = os.path.join('config', 'config.ini')
     config = configparser.ConfigParser()
-    if not os.path.isfile('config.ini'):
+    if not os.path.isfile(path):
         raise RuntimeError('No configuration file found!')
-    config.read('config.ini')
+    config.read(path)
     if not 'server' in config:
         raise RuntimeError('No server information found in configuration file!')
     server = Server(host=config['server']['host'], port=config['server']['port'])
