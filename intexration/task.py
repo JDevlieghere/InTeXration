bbb915630a9ce5bd4dfd761c10f380fb9bb9a409
JDevlieghere <info@jonasdevlieghere.com>
2014-01-16 19:50:12 +0100

    More logging.

--- intexration/task.py
+++ intexration/task.py
@@ -45,22 +45,23 @@ class Task:
     def _clone(self):
         if subprocess.call(['git', 'clone', self._url, self._build_dir]) != 0:
             raise RuntimeError('git clone failed!')
+        logger.debug("Cloned %s to &s", self._repository, self._build_dir)
 
     def _makeindex(self, file):
         with cd(self._build_dir):
             if subprocess.call(['makeindex', file], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL) != 0:
-                logger.warning('makeindex failed!')
+                logger.warning('Makeindex failed.')
 
     def _bibtex(self, file):
         with cd(self._build_dir):
             if subprocess.call(['bibtex', file], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL) != 0:
-                logger.warning('bibtex failed!')
+                logger.warning('Bibtex failed.')
 
     def _compile(self, file):
         with cd(self._build_dir):
             if subprocess.call(['pdflatex', '-interaction=nonstopmode', file], stdout=subprocess.DEVNULL,
                                stderr=subprocess.DEVNULL) != 0:
-                logger.warning('pdflatex compilation finished with errors!')
+                logger.error('Compilation (pdflatex) finished with errors.')
 
     def _copy(self, pdf_file, log_file):
         # PDF File
@@ -74,9 +75,10 @@ class Task:
 
     def _clean(self):
         shutil.rmtree(self._build_dir)
+        logger.debug("Directory %s cleaned.", self._build_dir)
 
     def run(self):
-        logger.info("Task started for " + self._repository)
+        logger.info("New InTeXRation task started for %s", self._repository)
         try:
             self._clone()
             self._compile('main.tex')
@@ -89,4 +91,3 @@ class Task:
         finally:
             self._clean()
         logger.info("Task finished for " + self._repository)
-
