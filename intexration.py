52a9c0dc869122da4b872afc920072c5a7e28703
JDevlieghere <info@jonasdevlieghere.com>
2014-01-16 18:38:06 +0100

    Automatically create api key file when not found

--- intexration.py
+++ intexration.py
@@ -6,10 +6,12 @@ from intexration.server import Server
 # Logger
 logger = logging.getLogger('intexration')
 
-# Application Constants
-api_keys_file = 'api_keys.txt'
+# Configuration Files
 config_folder = 'config'
+api_keys_file = 'api_keys.txt'
 config_file = 'config.ini'
+
+# Configuration Keys
 server_key = 'SERVER'
 host_key = 'host'
 port_key = 'port'
@@ -26,15 +28,14 @@ def main():
     if not os.path.isfile(config_path):
         raise RuntimeError('No configuration file found!')
     if not os.path.isfile(api_keys_path):
-        raise RuntimeError('No file with API keys found!')
+        open(api_keys_path, 'w+').close()
+        logger.warning("No API key file found. Empty file %s created in the configuration directory.", api_keys_file)
     config.read(config_path)
     if not server_key in config:
         raise RuntimeError('No server information found in configuration file!')
     server = Server(host=config[server_key][host_key], port=config[server_key][port_key], api_keys=api_keys_path)
-    try:
-        server.start()
-    except Exception as e:
-        logger.error(e)
+    server.start()
+
 
 if __name__ == '__main__':
     main()
