b6fbe1169944c1ad86b5e20dca1ce0d1d2f7263f
JDevlieghere <info@jonasdevlieghere.com>
2014-01-16 18:13:57 +0100

    Config file added.

--- intexration.py
+++ intexration.py
@@ -1,12 +1,20 @@
-import argparse
+import configparser
+import os
 from intexration.server import Server
 
+
 def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-host', help='hostname', default='localhost')
-    parser.add_argument('-port', help='port', default=8000)
-    args = parser.parse_args()
-    server = Server(host=args.host, port=args.port)
+    # parser = argparse.ArgumentParser()
+    # parser.add_argument('-host', help='hostname', default='localhost')
+    # parser.add_argument('-port', help='port', default=8000)
+    # args = parser.parse_args()
+    config = configparser.ConfigParser()
+    if not os.path.isfile('config.ini'):
+        raise RuntimeError('No configuration file found!')
+    config.read('config.ini')
+    if not 'server' in config:
+        raise RuntimeError('No server information found in configuration file!')
+    server = Server(host=config['server']['host'], port=config['server']['port'])
     server.start()
 
 if __name__ == '__main__':
