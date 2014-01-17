b6759d03974a2e70537b3127f23da9ad3142b838
JDevlieghere <info@jonasdevlieghere.com>
2014-01-18 00:16:17 +0100

    Setup.py added.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='InTeXration',
+    version='1.0.0',
+    packages=['intexration'],
+    url='https://github.com/JDevlieghere/InTeXration',
+    license='Apache License, Version 2.0',
+    author='Jonas Devlieghere',
+    author_email='info@jonasdevlieghere.com',
+    description='LaTeX Integration Service'
+)
