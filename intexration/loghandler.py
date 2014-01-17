a0191d4b340e35e5c70180a7a8c07aba5d666ead
JDevlieghere <info@jonasdevlieghere.com>
2014-01-17 17:18:30 +0100

    IO Reduced when reading log.

--- intexration/loghandler.py
+++ intexration/loghandler.py
@@ -4,19 +4,20 @@ import os
 
 class LogHandler:
     def __init__(self, path):
-        self._path = path
+        self._lines = self._lines(path)
 
-    def _lines(self):
-        if not os.path.isfile(self._path):
+    @staticmethod
+    def _lines(path):
+        if not os.path.isfile(path):
             raise RuntimeError("The logfile does not exist")
-        log_file = open(self._path, "r", encoding='latin-1')
+        log_file = open(path, "r", encoding='latin-1')
         return log_file.readlines()
 
     def get_warnings(self):
         warning_prefix = 'Warning'
         errors = []
         multi_line_error = False
-        for line in self._lines():
+        for line in self._lines:
             if multi_line_error and line == '\n':
                 multi_line_error = False
             if warning_prefix in line or multi_line_error:
@@ -28,7 +29,7 @@ class LogHandler:
         error_prefix = "! "
         errors = []
         multi_line_error = False
-        for line in self._lines():
+        for line in self._lines:
             if multi_line_error and line == '\n':
                 multi_line_error = False
             if line.startswith(error_prefix) or multi_line_error:
