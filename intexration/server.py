2c6ac59d468cc807cf09891c841a454cd48e044e
JDevlieghere <info@jonasdevlieghere.com>
2014-01-17 16:59:38 +0100

    Wrong template name fixed.

--- intexration/server.py
+++ intexration/server.py
@@ -66,4 +66,4 @@ class Server:
         file_name = name + '.log'
         path = os.path.join(os.getcwd(), 'out', repo, file_name)
         log_handler = LogHandler(path)
-        return template('templates/hello_template', repo=repo, name=name, errors=log_handler.get_errors(), warnings=[], all=log_handler.get_all())
+        return template('templates/log', repo=repo, name=name, errors=log_handler.get_errors(), warnings=[], all=log_handler.get_all())
