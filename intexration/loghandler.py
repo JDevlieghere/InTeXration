8cbf4071fcbed82d0e63c3bc3f6151cb6e995dce
JDevlieghere <info@jonasdevlieghere.com>
2014-01-17 17:20:22 +0100

    Minor fixes.

--- intexration/loghandler.py
+++ intexration/loghandler.py
@@ -4,10 +4,10 @@ import os
 
 class LogHandler:
     def __init__(self, path):
-        self._lines = self._lines(path)
+        self._lines = self._read_lines(path)
 
     @staticmethod
-    def _lines(path):
+    def _read_lines(path):
         if not os.path.isfile(path):
             raise RuntimeError("The logfile does not exist")
         log_file = open(path, "r", encoding='latin-1')
@@ -15,15 +15,15 @@ class LogHandler:
 
     def get_warnings(self):
         warning_prefix = 'Warning'
-        errors = []
+        warnings = []
         multi_line_error = False
         for line in self._lines:
             if multi_line_error and line == '\n':
                 multi_line_error = False
             if warning_prefix in line or multi_line_error:
-                errors.append(line)
+                warnings.append(line)
                 multi_line_error = True
-        return errors
+        return warnings
 
     def get_errors(self):
         error_prefix = "! "
