5ba43078088b987c9e2d5eeae8b2d6caf6b02fab
JDevlieghere <info@jonasdevlieghere.com>
2014-01-17 21:38:51 +0100

    Fixed crash when idx or bib file were missing from subdir.

--- intexration/task.py
+++ intexration/task.py
@@ -48,7 +48,7 @@ class Task:
         """Clone repository to build dir."""
         if subprocess.call(['git', 'clone', self._url, self._build_dir], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL) != 0:
             raise RuntimeError('git clone failed!')
-        logging.debug("Cloned %s to &s", self._repository, self._build_dir)
+        logging.debug("Cloned %s", self._repository)
 
     def _makeindex(self, file):
         """Make index."""
@@ -76,13 +76,18 @@ class Task:
         tex_dest_path = os.path.join(self._build_dir, tex_file)
         shutil.copyfile(tex_source_path, tex_dest_path)
         # idx File
-        idx_source_path = os.path.join(self._build_dir, dir, idx_file)
-        idx_dest_path = os.path.join(self._build_dir, idx_file)
-        shutil.copyfile(idx_source_path, idx_dest_path)
-        # Bibtex File
-        bib_source_path = os.path.join(self._build_dir, dir, bib_file)
-        bib_dest_path = os.path.join(self._build_dir, bib_file)
-        shutil.copyfile(bib_source_path, bib_dest_path)
+        try:
+            idx_source_path = os.path.join(self._build_dir, dir, idx_file)
+            idx_dest_path = os.path.join(self._build_dir, idx_file)
+            shutil.copyfile(idx_source_path, idx_dest_path)
+        except IOError:
+            logging.debug("No index file to copy.")
+        try:
+            bib_source_path = os.path.join(self._build_dir, dir, bib_file)
+            bib_dest_path = os.path.join(self._build_dir, bib_file)
+            shutil.copyfile(bib_source_path, bib_dest_path)
+        except IOError:
+            logging.debug("No BibTex file to copy.")
 
     def _copy(self, pdf_file, log_file):
         """Copy the PDF and Logfile to the output directory."""
@@ -102,7 +107,8 @@ class Task:
 
     def _build(self, build):
         """Build all."""
-        self._move(build.get_dir(), build.get_tex(), build.get_idx(), build.get_bib())
+        if build.get_dir() != '':
+            self._move(build.get_dir(), build.get_tex(), build.get_idx(), build.get_bib())
         self._compile(build.get_tex())
         self._makeindex(build.get_idx())
         self._bibtex(build.get_bib())
