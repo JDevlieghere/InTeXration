99522c873a2458ac389ac67047813c5e91ecb2bf
JDevlieghere <info@jonasdevlieghere.com>
2014-01-18 19:08:44 +0100

    Do not start server when configuring.

--- intexration/__main__.py
+++ intexration/__main__.py
@@ -19,23 +19,30 @@ def main():
     if not settings.all_files_exist():
         raise RuntimeError("Some necessary files were missing. Please consult the log.")
 
+    quit_when_done = False
     args = parser.parse_args()
     if args.host is not None:
         settings.set_config('SERVER', 'host', args.host)
         logging.info("Host changed to %s", args.host)
+        quit_when_done = True
     if args.port is not None:
         settings.set_config('SERVER', 'port', args.port)
+        quit_when_done = True
         logging.info("Port changed to %s", args.port)
     if args.add is not None:
         ApiHelper(settings.API_KEY_FILE).add(args.add)
         logging.info("API key added.")
+        quit_when_done = True
     if args.remove is not None:
         ApiHelper(settings.API_KEY_FILE).add(args.remove)
         logging.info("API key %s removed.", args.remove)
+        quit_when_done = True
     if args.list is not None:
         for line in ApiHelper(settings.API_KEY_FILE).get_all():
             print(line[0])
-
+        quit_when_done = True
+    if quit_when_done:
+        quit()
 
     server = Server(host=settings.get_config('SERVER', 'host'),
                     port=settings.get_config('SERVER', 'port'))
