be787bb38eeb6ce9d86e334b37757aae76097bdf
JDevlieghere <info@jonasdevlieghere.com>
2014-01-20 20:30:03 +0100

    Import and export configuration.

--- intexration/config.py
+++ intexration/config.py
@@ -3,6 +3,8 @@ import os
 import configparser
 
 # Directories
+import shutil
+
 PATH_ROOT = os.path.abspath(os.path.dirname(__file__))
 PATH_CONFIG = os.path.join(PATH_ROOT, 'config')
 PATH_DATA = os.path.join(PATH_ROOT, 'data')
@@ -13,7 +15,7 @@ PATH_BUILD = os.path.join(PATH_ROOT, 'build')
 
 # Files
 FILE_API_KEY = os.path.join(PATH_DATA, 'api_keys.csv')
-FILE_CONFI = os.path.join(PATH_CONFIG, 'settings.cfg')
+FILE_CONFIG = os.path.join(PATH_CONFIG, 'settings.cfg')
 FILE_LOGGING = os.path.join(PATH_CONFIG, 'logger.cfg')
 
 # Configuration Keys
@@ -26,11 +28,15 @@ LOG_NEW_LINE_CHAR = '\n'
 LOG_ERROR_STRING = '! '
 LOG_WARNING_STRING = 'Warning'
 
+# Config
+CONFIG_BASENAME = 'settings.cfg'
+
+
 def all_files_exist():
     if not os.path.exists(FILE_API_KEY):
         logging.error("File with API keys not found.")
         return False
-    if not os.path.exists(FILE_CONFI):
+    if not os.path.exists(FILE_CONFIG):
         logging.error("Configuration file not found.")
         return False
     if not os.path.exists(FILE_LOGGING):
@@ -41,16 +47,31 @@ def all_files_exist():
 
 def read(section, key):
     config = configparser.ConfigParser()
-    config.read(FILE_CONFI)
+    config.read(FILE_CONFIG)
     return config[section][key]
 
 
 def write(section, key, value):
     config = configparser.ConfigParser()
-    config.read(FILE_CONFI)
+    config.read(FILE_CONFIG)
     config.set(section, key, value)
-    with open(FILE_CONFI, 'w+') as configfile:
+    with open(FILE_CONFIG, 'w+') as configfile:
         config.write(configfile)
+
+
+def export_file(dir):
+    path = os.path.join(dir, CONFIG_BASENAME)
+    shutil.copyfile(FILE_CONFIG, path)
+    logging.info("Configuration exported to %s", path)
+
+
+def import_file(dir):
+    path = os.path.join(dir, CONFIG_BASENAME)
+    if not os.path.exists(path):
+        logging.error("Importing configuration failed: %s does not exist", CONFIG_BASENAME)
+        return
+    shutil.copyfile(path, FILE_CONFIG)
+    logging.info("Configuration imported from %s", path)
 
 # Server Root
 SERVER_ROOT = 'http://'+read('SERVER', 'host')+':'+read('SERVER', 'port')+'/'
