1ddc6435ebfef0695cda91069c66409702510a67
JDevlieghere <info@jonasdevlieghere.com>
2014-01-21 00:47:26 +0100

    Test directory added.

---
+++ tests/test_example.py
@@ -0,0 +1,11 @@
+import pytest
+
+
+class TestClass:
+    def test_one(self):
+        x = "this"
+        assert 'h' in x
+
+    def test_two(self):
+        x = "hello"
+        assert hasattr(x, 'check')
