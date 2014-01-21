19898d90a95adb738a8bbc44897c8ac91b0c08d9
JDevlieghere <info@jonasdevlieghere.com>
2014-01-21 22:20:44 +0100

    Bug found and fixed.

--- intexration/server.py
+++ intexration/server.py
@@ -37,6 +37,7 @@ class Server:
         payload = request.forms.get('payload')
         try:
             data = json.loads(payload)
+            logging.debug(data['ref'])
             if self.master_name in data['ref']:
                 owner = data['repository']['owner']['name']
                 repository = data['repository']['name']
