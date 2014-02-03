56317f864ddfc99d7f0add1b086414f972d163b6
JDevlieghere <info@jonasdevlieghere.com>
2014-02-03 01:17:04 +0100

    No cleanup for debugging.

--- intexration/task.py
+++ intexration/task.py
@@ -145,7 +145,7 @@ class CompileTask(Task):
         document = Document(self.identifier.name, self.build.path)
         document.move_to(self.document_directory)
         self.build_manager.submit_document(self.identifier, document)
-        self.build.finish()
+        #self.build.finish()
 
     def run(self):
         logging.info("Compiling %s", self.identifier)
