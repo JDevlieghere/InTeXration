0d1cfaca0c4fcf99cff90ba759577e979cf2fcab
JDevlieghere <info@jonasdevlieghere.com>
2014-02-11 19:30:42 +0100

    No more need for config parser.

--- intexration/parser.py
+++ intexration/parser.py
@@ -1,6 +1,5 @@
 import configparser
 import os
-import sys
 
 __author__ = 'Jonas'
 
@@ -42,65 +41,3 @@ class BuildParser:
         if self.parser.has_option(name, self.BIB):
             return self.parser[name][self.BIB]
         return name
-
-
-class RunArgumentParser:
-
-    def __init__(self, arguments, config, api_manager):
-        self.arguments = arguments
-        self.config = config
-        self.api_manager = api_manager
-        self.exit = False
-
-    def get(self, argument):
-        return getattr(self.arguments, argument)
-
-    def is_set(self, argument):
-        return hasattr(self.arguments, argument) and getattr(self.arguments, argument) is not None
-
-    def is_true(self, argument):
-        return hasattr(self.arguments, argument) and hasattr(self.arguments, argument)
-
-    def parse(self):
-        self._parse_config()
-        self._parse_api()
-        self._exit()
-
-    def _parse_config(self):
-        if self.is_set('config_host'):
-            self.config.write('SERVER', 'host', self.get('config_host'))
-            self._exit_on_finish()
-        if self.is_set('config_port'):
-            self.config.write('SERVER', 'port', self.get('config_port'))
-            self._exit_on_finish()
-        if self.is_set('config_export'):
-            self.config.file_export(self.get('config_export'))
-            self._exit_on_finish()
-        if self.is_set('config_import'):
-            self.config.file_import(self.get('config_import'))
-            self._exit_on_finish()
-
-    def _parse_api(self):
-        if self.is_set('api_add'):
-            self.api_manager.add_key(self.get('api_add'))
-            self._exit_on_finish()
-        if self.is_set('api_remove'):
-            self.api_manager.remove_key(self.get('api_remove'))
-            self._exit_on_finish()
-        if self.is_true('api_list'):
-            for line in self.api_manager.all_keys():
-                print(line[0])
-            self._exit_on_finish()
-        if self.is_set('api_export'):
-            self.api_manager.export_file(self.get('api_export'))
-            self._exit_on_finish()
-        if self.is_set('api_import'):
-            self.api_manager.import_file(self.get('api_import'))
-            self._exit_on_finish()
-
-    def _exit_on_finish(self):
-        self.exit = True
-
-    def _exit(self):
-        if self.exit:
-            sys.exit(0)
